32676093e633aef9d4ffc21e0d5f70ba56b26e64
pylagovardhansahithi25csm-ai <pylagovardhansahithi.25.csm@anits.edu.in>
2026-08-29 17:50:35 +0530

    Add files via upload

---
+++ Assignment/assignement2/Single.java
@@ -0,0 +1,23 @@
+class Add{
+    int a=10;
+    void demo(){
+        int x=20,y=30;
+        System.out.println( "addition:" +(a+x+y));
+
+    }
+}
+class Subtract extends Add{
+    int b=5;
+    void sample(){
+        System.out.println("subtract:" +(a-b));
+    }
+}
+   public class Single {
+    public static void main(String args[]){
+        Subtract s=new Subtract();
+        s.sample();
+        s.demo();
+
+    }
+ 
+}
