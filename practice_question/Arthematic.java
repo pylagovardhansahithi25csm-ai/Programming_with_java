3a6a1b93704a6468389abc19e7e0dc353298b6f9
pylagovardhansahithi25csm-ai <pylagovardhansahithi.25.csm@anits.edu.in>
2026-09-11 11:24:58 +0530

    Create Arthematic.java

---
+++ practice_question/Arthematic.java
@@ -0,0 +1,18 @@
+package project;
+public class Arthematic {
+	public static void main(String args[]) {
+	int a=10,b=20;
+	int add =(a+b);
+	System.out.println("add:"+ (a+b));	
+		try {
+			
+			int d=a/0;
+		}catch(ArithmeticException ae) {
+	
+		
+		System.out.println("cannot divide with zero:");
+}
+	
+	System.out.println("this a single catch:");
+}
+}
