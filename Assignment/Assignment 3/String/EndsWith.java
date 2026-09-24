5f5704e4382eeb949d9bb48ec6551bb3b8fec622
pylagovardhansahithi25csm-ai <pylagovardhansahithi.25.csm@anits.edu.in>
2026-09-24 15:28:20 +0530

    Create EndsWith.java

---
+++ Assignment/Assignment 3/String/EndsWith.java
@@ -0,0 +1,9 @@
+package Assignments;
+
+public class EndsWith{
+    public static void main(String[] args) {
+        String str = "Hello Java";
+
+        System.out.println("Starts with Hello: " + str.EndsWith("Hello"));
+
+    }
