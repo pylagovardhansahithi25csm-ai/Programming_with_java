468552eb29795b4ecf64ac270e1f3e4168255303
pylagovardhansahithi25csm-ai <pylagovardhansahithi.25.csm@anits.edu.in>
2026-09-24 15:38:54 +0530

    Create SubString.java

---
+++ Assignment/Assignment 3/String/SubString.java
@@ -0,0 +1,9 @@
+package Assignment;
+public class Substring {
+    public static void main(String[] args) {
+        String str = "Programming";
+
+        System.out.println("String: " + str);
+        System.out.println("Substring: " + str.substring(3, 7));
+    }
+}
